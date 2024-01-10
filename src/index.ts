b8e57da934c2b939fedb2e980e69465a2bda8263
Anurigwo Chibuzor <cibuzoraustine50@gmail.com>
2024-01-10 06:18:47 +0100

    Update explore api routes

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,41 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { Web5 } from "@web5/api";
+import cors from "cors"
 
 const { web5, did } = await Web5.connect(); 
 
 const app = express();
 const port = process.env.PORT || 5550;
 
-app.get('/explore', async (req:Request, res:Response) => {
+// set cors headers
+app.use(cors())
+
+// remove all json null response value
+app.set('json replacer', (k:any, v:any) => (v === null ? undefined : v))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/api/explore', async (req:Request, res:Response) => {
     try {
         const resp = await web5.dwn.records.query({
             from: did,
             message: {
                 filter: {
-                    schema: 'w5p.backend/protocol/schema',
+                    schema: 'w5p.protocol/schemas/exploreSchema',
                     dataFormat: 'application/json'
                 }
             }
         })
         if (resp.records) {
-            const records = resp.records.map(async (e) => {
+            const records = await Promise.all(resp.records.map(async (e) => {
                 const data = await e.data.json();
-                return data
-            })
+                return {
+                    id: e.id,
+                    ...data
+                }
+            }))
             return res.json(records)
         }else {
             return res.json([])
@@ -33,22 +46,42 @@ app.get('/explore', async (req:Request, res:Response) => {
     }
 });
 
-app.post('/publish', async (req:Request, res:Response) => {
+app.post('/api/publish', async (req:Request, res:Response) => {
+    if (!req.body.title || !req.body.recordId || !req.body.author) {
+        return res.status(422).json({message: "at least one field was left undefined"})
+    }
     try {
         const resp = await web5.dwn.records.create({
             data: {
                 title: req.body.title,
-                recordId: req.body.id,
+                recordId: req.body.recordId,
+                author: req.body.author
             },
             message: {
-                schema: 'w5p.backend/protocol/schema',
-                dataFormat: 'application/json'
+                schema: 'w5p.protocol/schemas/exploreSchema',
+                dataFormat: 'application/json',
+                recipient: req.body.author,
+                published: true
             }
         })
-    
+        resp.record?.send(did);
         return res.json(resp)
     } catch (error) {
         return res.status(400).json({message: "error occured while saving to dwn"})
+    }
+});
+
+app.get('/api/protocol/delete/:id', async (req:Request, res:Response) => {
+    try {
+        const resp = await web5.dwn.records.delete({
+            from: did,
+            message: {
+                recordId: req.params.id
+            }
+        })
+        return res.json(resp.status);
+    } catch (error) {
+        return res.status(400).json({message: "error occured while fetching from dwn"})
     }
 });
 
